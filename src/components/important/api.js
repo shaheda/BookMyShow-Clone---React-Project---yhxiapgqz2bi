536863aa2cebf3bd60db138a81c44ecc07748fbc
shaheeda <shaheedashaikh23@gmail.com>
2023-01-21 21:48:54 +0530

    final change

---
+++ src/components/important/api.js
@@ -0,0 +1,8 @@
+export const apiUrl = {
+    key : "3f8245cec6ed13b0d9466b7a244dfe51",
+    base : "https://api.themoviedb.org/3/",
+    getGenres : "genre/movie/list",
+    nowPlaying : "movie/now_playing",
+    language : "language=en-US",
+    imageBase : "https://image.tmdb.org/t/p/w500"
+}
